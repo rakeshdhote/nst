e64d38b0e87f9bd7fd2e2d8dc65fdaaab1206ed8
Sharvari Dhote <drsharvaridhote@gmail.com>
2024-11-29 23:00:30 -0500

    github actions added

--- script.sh
+++ script.sh
@@ -18,7 +18,15 @@ pnpm tauri init
 #dev
 pnpm tauri dev
 
+# git init
+git init
 
+# git add
+git add .
+git commit -m "init"
+git remote add origin https://github.com/rakeshdhote/nst.git
+git branch -M main
+git push -u origin main
 
-
-
+# git tag and push
+git tag v1.0.0 && git push origin v1.0.0
