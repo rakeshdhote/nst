8d091c0a0f272a1ba205ba9bd0a9cf9949b352ca
Sharvari Dhote <drsharvaridhote@gmail.com>
2024-11-29 23:05:01 -0500

    updates script

--- script.sh
+++ script.sh
@@ -31,3 +31,8 @@ git push -u origin main
 
 # git tag and push
 git tag v1.0.0 && git push origin v1.0.0
+git push -u origin maingit rm .github/workflows/release.yml
+git commit -m "Remove workflow file to push first"
+git push -u origin main
+git tag v0.0.1
+git push origin v0.0.1
