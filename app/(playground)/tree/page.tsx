418eeb11a7b1d26bd23f3ce925e74456336446e5
rakeshdhote <drsharvaridhote@gmail.com>
2024-12-18 23:52:04 -0500

    prefect_workbook working with openrouter

--- app/(playground)/tree/page.tsx
+++ app/(playground)/tree/page.tsx
@@ -1,37 +1,70 @@
 "use client";
 
-import { UncontrolledTreeEnvironment, StaticTreeDataProvider, Tree } from 'react-complex-tree'; 
+import {
+    UncontrolledTreeEnvironment,
+    Tree,
+    StaticTreeDataProvider,
+  } from "react-complex-tree";
 import { longTree } from './treeData'; // Import the tree data
-import 'react-complex-tree/lib/style-modern.css';
-const TreeComponent = () => {
-    return (
-        <>
-            <UncontrolledTreeEnvironment
-                canDragAndDrop={true}
-                canDropOnFolder={true}
-                canReorderItems={true}
-                dataProvider={new StaticTreeDataProvider(longTree.items, (item, data) => ({ ...item, data }))}
-                getItemTitle={item => item.data}
-                viewState={{}}
-            >
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'space-evenly',
-                        alignItems: 'baseline',
-                        padding: '20px 0',
-                    }}
-                >
-                    <div style={{ width: '28%', backgroundColor: 'white' }}>
-                        <Tree treeId="tree-1" rootItem="root" treeLabel="Tree 1" />
-                    </div>
-                    <div style={{ width: '28%', backgroundColor: 'white' }}>
-                        <Tree treeId="tree-2" rootItem="root" treeLabel="Tree 2" />
-                    </div>
-                </div>
-            </UncontrolledTreeEnvironment>
-        </>
-    );
-};
+import { JSX } from 'react';
+import "react-complex-tree/lib/style-modern.css";
 
-export default TreeComponent;
+// export default function TreeComponent(): JSX.Element {
+//     return (
+//         <>
+//             <UncontrolledTreeEnvironment
+//                 canDragAndDrop={true}
+//                 canDropOnFolder={true}
+//                 canReorderItems={true}
+//                 canSearch={true}
+//                 canRename={true}
+//                 dataProvider={new StaticTreeDataProvider(longTree.items, (item, data) => ({ ...item, data }))}
+//                 getItemTitle={item => item.data}
+//                 viewState={{ 'tree-1': {}, 'tree-2': {} }}
+//             >
+//                 <div
+//                     style={{
+//                         display: 'flex',
+//                         justifyContent: 'space-evenly',
+//                         alignItems: 'baseline',
+//                         padding: '20px 0',
+//                     }}
+//                 >
+//                     <div style={{ width: '28%', backgroundColor: 'white' }}>
+//                         <Tree treeId="tree-1" rootItem="root" treeLabel="Tree 1" />
+//                     </div>
+//                     <div style={{ width: '28%', backgroundColor: 'white' }}>
+//                         <Tree treeId="tree-2" rootItem="root" treeLabel="Tree 2" />
+//                     </div>
+//                 </div>
+//             </UncontrolledTreeEnvironment>
+//         </>
+//     );
+// };
+
+// export default TreeComponent;
+
+export default function TreeComponent() {
+    return (
+      <UncontrolledTreeEnvironment
+        dataProvider={
+          new StaticTreeDataProvider(longTree.items, (item, data) => ({
+            ...item,
+            data,
+          }))
+        }
+        getItemTitle={(item) => item.data}
+        canDragAndDrop={true}
+        canReorderItems={true}
+        canDropOnFolder={true}
+        canDropOnNonFolder={true}
+        viewState={{
+          "tree-1": {
+            expandedItems: ["Fruit"],
+          },
+        }}
+      >
+        <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+      </UncontrolledTreeEnvironment>
+    );
+  }
